9cbab47ef52a7acbf9630e561414b322af84cd35
Rosário Baiano <aleixobaiano@icloud.com>
2024-11-14 15:06:22 +0100

    adicionando arquivos do sistema frontend

---
+++ SRC/cadastro.js
@@ -0,0 +1,102 @@
+﻿window.onload = function (e) {
+
+    var btnCadastrar = document.getElementById("Cadastrar");
+
+    var txtNome = document.getElementById("txtNome");
+
+    var txtSobrenome = document.getElementById("txtSobrenome");
+
+    var txtEmail = document.getElementById("txtEmail");
+
+    var txtTelefone = document.getElementById("txtTelefone");
+
+    var SLGenero = document.getElementById("SlGenero");
+
+    var txtSenha = document.getElementById("txtSenha");
+
+    txtNome.focus();
+
+    btnCadastro.onclick = function (e) {
+
+        e.preventDefault();
+
+        var nome = txtNome.value;
+
+        var sobrenome = txtSobrenome.value;
+
+        var email = txtEmail.value;
+
+        var telefone = txtTelefone.value;
+
+        var genero = SLGenero.value;
+
+        var senha = txtSenha.value;
+
+        if (nome == "") {
+            exibirMensagemErro("Informe nome.");
+        }
+        else if (sobrenome == "") {
+            exibirMensagemErro("Informe o sobrenome.");
+        }
+        else if (email == "") {
+            exibirMensagemErro("Informe o email.");
+        }
+        else if (telefone == "") {
+            exibirMensagemErro("Informe o telefone.");
+        }
+        else if (senha == "") {
+            exibirMensagemErro("Informe a senha.")
+        }
+        else {
+            realizarCadastro(nome, sobrenome, email, telefone, genero, senha);
+        }
+    };
+    function exibirMensagemErro(mensagem) {
+
+        var spnErro = document.getElementById("spnErro");
+
+        spnErro.innerText = mensagem;
+
+        spnErro.style.display = "block";
+
+        setTimeout(function () {
+            spnErro.style.display = "none";
+        }, 5000);
+    }
+    function realizarCadastro(nome, sobrenome, email, telefone, genero, senha) {
+
+        var data = JSON.stringify({
+            "nome": nome,
+            "sobrenome": sobrenome,
+            "email": email,
+            "senha": senha,
+            "telefone": telefone,
+            "genero": genero
+        });
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+
+                var result = JSON.parse(this.responseText);
+
+                if (result.sucesso) {
+
+                    localStorage.setItem("usuarioGuid", result.usuarioGuid);
+
+                    window.location.href = 'home.html';
+                }
+                else {
+                    exibirMensagemErro(result.mensagem);
+                }
+            }
+        });
+
+        xhr.open("POST", "https://localhost:44332/api/usuario/Cadastro");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+    }
+}
---
+++ SRC/esqueceu-senha.js
@@ -0,0 +1,66 @@
+﻿window.onload = function (e) {
+
+    var btnEsqueceu = document.getElementById("btnEsqueceu");
+
+    var txtEmail = document.getElementById("txtEmail");
+
+    txtEmail.focus();
+
+    btnEsqueceu.onclick = function (e) {
+
+        e.preventDefault();
+
+        var email = txtEmail.value;
+
+        if (email == "") {
+            exibirMensagemErro("Email obrigatório.");
+
+        }
+        else {
+            esqueceuSenha(email);
+        }
+    };
+    function exibirMensagemErro(mensagem) {
+
+        var spnErro = document.getElementById("spnErro");
+
+        spnErro.innerText = mensagem;
+
+        spnErro.style.display = "block";
+
+        setTimeout(function () {
+            spnErro.style.display = "none";
+        }, 5000);
+    }
+    function esqueceuSenha(email) {
+
+        var data = JSON.stringify({
+            "email": email
+        });
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+
+                var result = JSON.parse(this.responseText);
+
+                if (result.sucesso) {
+                    alert("E-mail enviado com sucesso!");
+                    
+                }
+                else {
+                    exibirMensagemErro(result.mensagem);
+                }
+
+                txtEmail.value = "";
+            }
+        });
+
+        xhr.open("POST", "https://localhost:44332/api/usuario/Esqueceu");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+    }
+}
